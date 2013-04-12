46b8f5bc7b4cfea86302aa366201a33537756b64
Jedabero <jedabero@hotmail.com>
2013-04-11 20:23:43 -0500

    Main method added... I feel so silly
    Signed-off-by: Jedabero <jedabero@hotmail.com>

---
+++ MetodosNumericos/src/ui/main/Metodos.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ui.main;
+
+/**
+ * @author Jedabero
+ *
+ */
+public class Metodos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
