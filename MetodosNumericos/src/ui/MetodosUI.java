69f155b51c30c2eb605160bff8c23abaa19dbbe9
Jedabero <jedabero@hotmail.com>
2013-04-25 17:42:50 -0500

    Added the class Add to this project... Oh menus
    Signed-off-by: Jedabero <jedabero@hotmail.com>

--- MetodosNumericos/src/ui/MetodosUI.java
+++ MetodosNumericos/src/ui/MetodosUI.java
@@ -4,6 +4,10 @@
 package ui;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+import resources.Add;
 
 /**
  * @author Jedabero
@@ -15,7 +19,9 @@ public class MetodosUI extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = 6637458677520322115L;
-
+	
+	private JMenuBar mnbPrincipal = new JMenuBar();
+	
 	/**
 	 * 
 	 */
@@ -24,16 +30,29 @@ public class MetodosUI extends JFrame{
 		setSize(1000, 700);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		initBarraMenu();
+		
 		
 		setLocationRelativeTo(getRootPane());
 		setVisible(true);
 	}
 	
-	
-	
-	
-	
-	
+	private void initBarraMenu() {
+		
+		//Acciones
+		JMenu menuAcciones = new JMenu("Acciones...");
+		menuAcciones.setMnemonic('A');
+		Add.menuItem(menuAcciones, "Polinomios", "Grafica y obtén las raices de un polinomio", null, 'P');
+		
+		
+		
+	}
+
+
+
+
+
+
 	/**
 	 * @param args
 	 */
