401eb38ff53d331b3bcbc5eb96697640fcf9915d
Jedabero <jedabero@hotmail.com>
2013-09-28 15:19:46 -0500

    Es mejor editar cada termino individualmente
    Signed-off-by: Jedabero <jedabero@hotmail.com>

---
+++ MetodosNumericos/src/ui/editors/EditaTerminoPanel.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ui.editors;
+
+import javax.swing.JPanel;
+
+/**
+ * @author jedabero
+ *
+ */
+public class EditaTerminoPanel extends JPanel {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7836520899017987399L;
+
+	/**
+	 * 
+	 */
+	public EditaTerminoPanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
