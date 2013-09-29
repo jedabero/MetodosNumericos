fa187d26de511227cdb14260456679cb8f0e9551
Jedabero <jedabero@hotmail.com>
2013-09-29 17:51:27 -0500

    FuncTrigPanel added. Should have made a branch first!
    Signed-off-by: Jedabero <jedabero@hotmail.com>

---
+++ MetodosNumericos/src/ui/editors/FuncTrigPanel.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package ui.editors;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+/**
+ * @author jedabero
+ *
+ */
+public class FuncTrigPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7580284096836385180L;
+
+	/**
+	 * 
+	 */
+	public FuncTrigPanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param layout
+	 */
+	public FuncTrigPanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param isDoubleBuffered
+	 */
+	public FuncTrigPanel(boolean isDoubleBuffered) {
+		super(isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param layout
+	 * @param isDoubleBuffered
+	 */
+	public FuncTrigPanel(LayoutManager layout, boolean isDoubleBuffered) {
+		super(layout, isDoubleBuffered);
+		// TODO Auto-generated constructor stub
+	}
+
+}
