1b37e283ab62055436124fccbfa3fb661a13a768
Jedabero <jedabero@hotmail.com>
2013-09-30 17:23:18 -0500

    Done with the especial editors.
    Signed-off-by: Jedabero <jedabero@hotmail.com>

--- MetodosNumericos/src/ui/editors/FuncTrigPanel.java
+++ MetodosNumericos/src/ui/editors/FuncTrigPanel.java
@@ -10,11 +10,13 @@ import java.math.BigDecimal;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
 
 import resources.Add;
 import resources.math.Constantes.FuncionTrig;
+import resources.math.funciones.Termino;
 
 /**
  * @author jedabero
@@ -32,8 +34,12 @@ public class FuncTrigPanel extends JPanel implements Editors {
 
 	private int index;
 	
+	private CoeficientePanel cpA;
+	private BigDecimal coefA;
 	private CoeficientePanel cpB;
 	private BigDecimal coefB;
+	
+	private Termino termino;
 	
 	/**
 	 * 
@@ -44,24 +50,32 @@ public class FuncTrigPanel extends JPanel implements Editors {
 	
 	/**
 	 * 
+	 * @param ft
 	 */
 	public FuncTrigPanel(FuncionTrig ft) {
-		this(ft,BigDecimal.ZERO);
+		this(ft, BigDecimal.ZERO, BigDecimal.ZERO);
 	}
 
 	/**
 	 * 
+	 * @param ft
+	 * @param a
+	 * @param b
 	 */
-	public FuncTrigPanel(FuncionTrig ft, BigDecimal b) {
-		this(ft, b, -1);
+	public FuncTrigPanel(FuncionTrig ft, BigDecimal a, BigDecimal b) {
+		this(ft, a, b, -1);
 	}
 	
 	/**
 	 * 
+	 * @param ft
+	 * @param b
+	 * @param i
 	 */
-	public FuncTrigPanel(FuncionTrig ft, BigDecimal b, int i) {
+	public FuncTrigPanel(FuncionTrig ft, BigDecimal a, BigDecimal b, int i) {
 		super(new GridBagLayout());
 		this.tipoTrig = ft;
+		this.coefA = a;
 		this.coefB = b;
 		this.index = i;
 		
@@ -81,44 +95,60 @@ public class FuncTrigPanel extends JPanel implements Editors {
 		listaTipoTrig.setSelectedItem(tipoTrig);
 		listaTipoTrig.addItemListener(this);
 		
+		cpA = new CoeficientePanel("<html>A"+(index>=0?("<sub>"+index+"</sub>"):"")+"= </html>");
+		cpA.setTexto(coefA.toString());
 		cpB = new CoeficientePanel("<html>B"+(index>=0?("<sub>"+index+"</sub>"):"")+"= </html>");
 		cpB.setTexto(coefB.toString());
+		
+		termino = Termino.trigonometrico(tipoTrig, coefA, coefB, null);
 	}
 	
 	@Override
 	public void addComponents() {
-		System.out.println("YES");
-		Add.componente(this, new JLabel("Funcion",JLabel.CENTER),
+		Add.componente(this, new JLabel("Funcion:",JLabel.CENTER),
 				0, 0, 1, 1, 1.0, 1.0, GridBagConstraints.NONE, "");
 		Add.componente(this, listaTipoTrig,
 				1, 0, 3, 1, 1.0, 1.0, GridBagConstraints.BOTH, "");
+		Add.componente(this, cpA,
+				0, 1, 2, 1, 1.0, 1.0, GridBagConstraints.BOTH, "Coeficiente A");
 		Add.componente(this, cpB,
-				0, 1, 4, 1, 1.0, 1.0, GridBagConstraints.BOTH, "");
-		System.out.println("YES");
-	}
-
-	public static void main(String args[]) {
-		javax.swing.JFrame jf = new javax.swing.JFrame("FuncTrig Test");
-		jf.add(new FuncTrigPanel());
-		jf.setSize(300, 100);
-		jf.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-		jf.setVisible(true);
+				2, 1, 2, 1, 1.0, 1.0, GridBagConstraints.BOTH, "Coeficiente B");
 	}
 	
-	
+	public Termino getTermino() {
+		String textA = cpA.getTexto();
+		try {
+			coefA = new BigDecimal(textA);
+		} catch (Exception e) {
+			String msgstr = "<html>Coeficiente A" + (index>=0?("<sub>"+index+"</sub>"):"")
+					+ ": "+(textA.isEmpty()?"vacio":textA) + "</html>"; 
+			JOptionPane.showMessageDialog(this, msgstr, "Error", JOptionPane.WARNING_MESSAGE);
+			coefA = null;
+		}
+		
+		String textB = cpB.getTexto();
+		try {
+			coefB = new BigDecimal(textB);
+		} catch (Exception e) {
+			String msgstr = "<html>Coeficiente B" + (index>=0?("<sub>"+index+"</sub>"):"")
+					+ ": "+(textB.isEmpty()?"vacio":textB) + "</html>"; 
+			JOptionPane.showMessageDialog(this, msgstr, "Error", JOptionPane.WARNING_MESSAGE);
+			coefB = null;
+		}
+		
+		termino = (coefB==null||coefA==null) ? null : Termino.trigonometrico(tipoTrig, coefA, coefB, null);
+		
+		return termino;
+	}
 	
 	@Override
-	public void stateChanged(ChangeEvent e) {
-		
-		
-	}
+	public void stateChanged(ChangeEvent e) {}
 
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		if(e.getStateChange()== ItemEvent.SELECTED) {
 			tipoTrig = (FuncionTrig)e.getItem();
 		}
-		
 	}
 	
 }
---
+++ MetodosNumericos/src/ui/editors/MonomioPanel.java
@@ -0,0 +1,130 @@
+/**
+ * 
+ */
+package ui.editors;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ItemEvent;
+import java.math.BigDecimal;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+
+import resources.Add;
+import resources.math.funciones.Termino;
+
+/**
+ * @author jedabero
+ *
+ */
+public class MonomioPanel extends JPanel implements Editors {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7166446275028262369L;
+
+	private int index;
+	
+	private int grado;
+	private SpinnerNumberModel snmGrado;
+	private JSpinner spnrGrado;
+	
+	private CoeficientePanel cpA;
+	private BigDecimal coefA;
+	
+	private Termino termino;
+	
+	/**
+	 * Crea un panel para editar un termino tipo polinomico.
+	 * Por deafult el grado es 1 y el coeficiente es 0.
+	 */
+	public MonomioPanel() {
+		this(1);
+	}
+	
+	/**
+	 * Crea un panel para editar un termino tipo polinomico de grado g.
+	 * Por deafult el coeficiente es 0.
+	 * @param g grado del termino
+	 */
+	public MonomioPanel(int g) {
+		this(g, BigDecimal.ZERO);
+	}
+	
+	/**
+	 * Crea un panel para editar un termino tipo polinomico de grado g y coeficiente a
+	 * 
+	 * @param g grado del termino
+	 * @param a coeficiente
+	 */
+	public MonomioPanel(int g, BigDecimal a) {
+		this(g, a, -1);
+	}
+	
+	/**
+	 * 
+	 * @param g
+	 * @param a
+	 * @param i
+	 */
+	public MonomioPanel(int g, BigDecimal a, int i) {
+		super(new GridBagLayout());
+		this.grado = g<1 ? 1 : g;
+		this.coefA = a;
+		this.index = i;
+		
+		init();
+		addComponents();
+	}
+	
+	@Override
+	public void init() {
+		snmGrado = new SpinnerNumberModel(grado, 1, 25, 1);
+		spnrGrado = new JSpinner(snmGrado);
+		spnrGrado.addChangeListener(this);
+		
+		cpA = new CoeficientePanel("<html>A"+(index>=0?("<sub>"+index+"</sub>"):"")+"= </html>");
+		cpA.setTexto(coefA.toString());
+	}
+
+	@Override
+	public void addComponents() {
+		Add.componente(this, new JLabel("Grado:",JLabel.CENTER),
+				0, 0, 1, 1, 0.0, 1.0, GridBagConstraints.NONE, "");
+		Add.componente(this, spnrGrado,
+				1, 0, 2, 1, 1.0, 1.0, GridBagConstraints.BOTH, "");
+		Add.componente(this, cpA,
+				0, 1, 3, 1, 1.0, 1.0, GridBagConstraints.BOTH, "Coeficiente A");
+	}
+	
+	public Termino getTermino() {
+		String textA = cpA.getTexto();
+		try {
+			coefA = new BigDecimal(textA);
+		} catch (Exception e) {
+			String msgstr = "<html>Coeficiente A" + (index>=0?("<sub>"+index+"</sub>"):"")
+					+ ": "+(textA.isEmpty()?"vacio":textA) + "</html>"; 
+			JOptionPane.showMessageDialog(this, msgstr, "Error", JOptionPane.WARNING_MESSAGE);
+			coefA = null;
+		}
+		
+		termino = coefA==null ? null : Termino.monomio(grado, coefA, null);
+		
+		return termino;
+	}
+	
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		grado = Integer.parseInt(spnrGrado.getValue().toString());//(int)spnr.getValue();
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {}
+
+}
